a0c77856f4bbdfb21aed4b6032313f0c1f9dc210
rb-corretor <rudah04@hotmail.com>
2026-08-30 00:57:25 -0300

    Add files via upload

---
+++ compare.js
@@ -0,0 +1,1 @@
+export default async function handler(req,res){res.status(200).json({common:[],onlyA:[],onlyB:[],summary:{common:0,onlyA:0,onlyB:0},note:'Comparador pronto para a carga da rede.'});}
